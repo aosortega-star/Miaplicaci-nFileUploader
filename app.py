795ec8386210702efe49bcfd726d98d932142416
aosortega-star <aos.ortega@gmail.com>
2026-04-20 22:29:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,20 @@ import pandas as pd
 st.title("Manejo de Dataframes")
 st.sidebar.title("Herramientas")
 
-
 archivo = st.sidebar.file_uploader("Seleccione su archivo")
 
-datos = pd.read_csv(archivo)
+if archivo is not None:
 
-st.write(datos)
+    st.write("Su archivo ha sido cargado")
+    
+
+    if archivo.name.endswith(".csv"):
+        datos = pd.read_csv(archivo)
+    elif archivo.name.endswith(".xlsx"):
+        datos = pd.read_excel(archivo)
+      
+    st.write(datos)
+  
+else:
+    
+    st.write("Cargue el archivo ")
